199d6b02d808c3e82cbbaf9c6782cda5f312f5a4
Luis Chilo <gfxargentina@gmail.com>
2022-09-27 22:08:01 -0300

    add: car controller and route

--- src/controllers/car.controller.js
+++ src/controllers/car.controller.js
@@ -2,39 +2,92 @@ const carModel = require('../database/models').Car;
 const userModel = require('../database/models').User;
 
 const getAllCars = async (req, res) => {
-  carModel
-    .findAll({
+  // carModel
+  //   .findAll({
+  //     include: [{ model: userModel }],
+  //   })
+  //   .then((data) => {
+  //     res.json(data);
+  //   })
+  //   .catch((error) => {
+  //     res.json(error);
+  //   });
+
+  try {
+    const allCars = await carModel.findAll({
       include: [{ model: userModel }],
-    })
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((error) => {
-      res.json(error);
     });
+    res.status(200).json(allCars);
+  } catch (error) {
+    return res.status(500).json({ msg: error.message });
+  }
+};
+
+const getCarById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const car = await carModel.findByPk(id);
+    if (!car) {
+      return res.status(404).json({ msg: 'El auto no existe' });
+    }
+
+    res.status(200).json({ car });
+  } catch (error) {
+    return res.status(500).json({ msg: error.message });
+  }
 };
 
 const createCar = async (req, res) => {
-  carModel
-    .create(req.body)
-    .then((data) => {
-      res.json({ datos: data });
-    })
-    .catch((error) => {
-      res.json({ error: error });
-    });
+  // carModel
+  //   .create(req.body)
+  //   .then((data) => {
+  //     res.json({ datos: data });
+  //   })
+  //   .catch((error) => {
+  //     res.json({ error: error });
+  //   });
+
+  const newCar = await carModel.create(req.body);
+  res.status(200).json(newCar);
 };
 
 const updateCar = async (req, res) => {
-  res.json('actualizar un auto');
+  try {
+    const { id } = req.params;
+    const newCar = req.body;
+
+    await carModel.update(newCar, {
+      where: { id },
+    });
+
+    const updatedCar = await carModel.findByPk(id);
+
+    res.status(200).json({
+      msg: 'Auto Actualizado',
+      updatedCar,
+    });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 };
 
 const deleteCar = async (req, res) => {
-  res.json('borra un auto');
+  try {
+    const { id } = req.params;
+    await carModel.destroy({
+      where: { id },
+    });
+    res.status(200).json({
+      msg: 'Auto eliminado exitosamente',
+    });
+  } catch (error) {
+    return res.status(500).json({ msg: error.message });
+  }
 };
 
 module.exports = {
   getAllCars,
+  getCarById,
   createCar,
   updateCar,
   deleteCar,
--- src/routes/car.routes.js
+++ src/routes/car.routes.js
@@ -1,15 +1,20 @@
 const { Router } = require('express');
 const {
   getAllCars,
+  getCarById,
   createCar,
   updateCar,
   deleteCar,
 } = require('../controllers/car.controller');
 const router = Router();
 
-router.get('/', getAllCars);
-router.post('/', createCar);
-router.put('/', updateCar);
-router.delete('/', deleteCar);
+const validateJWT = require('../middleware/validate-jwt');
+const { isAdminRole, hasARole } = require('../middleware/validate-role');
+
+router.get('/', validateJWT, hasARole('ADMIN', 'USER'), getAllCars);
+router.get('/:id', validateJWT, hasARole('ADMIN', 'USER'), getCarById);
+router.post('/', validateJWT, hasARole('ADMIN', 'USER'), createCar);
+router.put('/:id', validateJWT, hasARole('ADMIN', 'USER'), updateCar);
+router.delete('/:id', validateJWT, hasARole('ADMIN', 'USER'), deleteCar);
 
 module.exports = router;
